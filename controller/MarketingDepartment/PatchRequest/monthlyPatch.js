30b066d5f1a1dea307596e0a4d9315161a037470
iamfemiab <abitogun22@gmail.com>
2021-06-23 19:06:58 -0700

    persisted edit table re.body to suscriber object in the database

--- controller/MarketingDepartment/PatchRequest/monthlyPatch.js
+++ controller/MarketingDepartment/PatchRequest/monthlyPatch.js
@@ -4,19 +4,132 @@ const Suscriber = require('../../../model/Management_team/Marketing_department/m
 
 
 
+let _suscriber = new Suscriber();
+
+
 const editMonthlySuscriber = async (req, res) => {
     try {
-        // const suscriber = await Suscriber.findById(req.params.id);
-        // if (!suscriber)
-        //     return res.status(500).json({
-        //         errorMessage: "can not find users"
-        //     })
-        // return res.status(200).json(suscriber);
 
-        console.log(" edit fields"+req.params.id)
-        
-        return res.status(200).json();
+        const suscriber = await Suscriber.findById(req.params.id);
+
+        let {
+            referalCode,
+            branch,
+            formNo,
+            fullName,
+            residentialAddress,
+            email,
+            phone,
+            occupation,
+            maritalStatus,
+            religion,
+            gender,
+            birthday,
+            permanentHomeAddress,
+            stateOfOrigin,
+            LGA,
+            homeTown,
+            prefferDaysOfMeeting,
+            contributionPlan,
+            bankName,
+            accountNumber,
+            BVN,
+            meansOfIdentification,
+            idCardNo,
+            kinFullname,
+            kinAddress,
+            kinEmail,
+            kinPhone,
+            kinOccupation,
+            kinOfficeAddress,
+            kinRelationshipType,
+            kinYearOfrelationship,
+            referee1FullName,
+            referee1HomeAddress,
+            referee1WorkAddress,
+            referee1Business,
+            referee1Email,
+            referee1Religion,
+            referee1Phone,
+            referee1Relationship,
+            referee2FullName,
+            referee2HomeAddress,
+            referee2WorkAddress,
+            referee2Business,
+            referee2Email,
+            referee2Religion,
+            referee2Phone,
+            referee2Relationship
+
+        } = req.body;
+
+        suscriber.referalCode = referalCode,
+            suscriber.branch = branch
+        suscriber.formNo = formNo
+        suscriber.fullName = fullName
+        suscriber.residentialAddress = residentialAddress
+        suscriber.email = email
+        suscriber.phone = phone
+        suscriber.occupation = occupation
+        suscriber.maritalStatus = maritalStatus
+        suscriber.religion = religion
+        suscriber.gender = gender
+        suscriber.birthday = birthday
+        suscriber.permanentHomeAddress = permanentHomeAddress
+        suscriber.stateOfOrigin = stateOfOrigin
+        suscriber.LGA = LGA
+        suscriber.homeTown = homeTown
+        suscriber.prefferDaysOfMeeting = prefferDaysOfMeeting
+        suscriber.contributionPlan = contributionPlan
+        suscriber.bankName = bankName
+        suscriber.accountNumber = accountNumber
+        suscriber.BVN = BVN
+        suscriber.meansOfIdentification = meansOfIdentification
+        suscriber.idCardNo = idCardNo
+        suscriber.kinFullname = kinFullname
+        suscriber.kinAddress = kinAddress
+        suscriber.kinEmail = kinEmail
+        suscriber.kinPhone = kinPhone
+        suscriber.kinOccupation = kinOccupation
+        suscriber.kinOfficeAddress = kinOfficeAddress
+        suscriber.kinRelationshipType = kinRelationshipType
+        suscriber.kinYearOfrelationship = kinYearOfrelationship
+        suscriber.referee1FullName = referee1FullName
+        suscriber.referee1HomeAddress = referee1HomeAddress
+        suscriber.referee1WorkAddress = referee1WorkAddress
+        suscriber.referee1Business = referee1Business
+        suscriber.referee1Email = referee1Email
+        suscriber.referee1Religion = referee1Religion
+        suscriber.referee1Phone = referee1Phone
+        suscriber.referee1Relationship = referee1Relationship
+        suscriber.referee2FullName = referee2FullName
+        suscriber.referee2HomeAddress = referee2HomeAddress
+        suscriber.referee2WorkAddress = referee2WorkAddress
+        suscriber.referee2Business = referee2Business
+        suscriber.referee2Email = referee2Email
+        suscriber.referee2Religion = referee2Religion
+        suscriber.referee2Phone = referee2Phone
+        suscriber.referee2Relationship = referee2Relationship;
+
+        if (_suscriber.customerImagePath !== "" || _suscriber.customerImagePath !== null) {
+            suscriber.customerImagePath = _suscriber.customerImagePath;
+        }
+
+        if (_suscriber.referee2ImagePath !== "" || _suscriber.referee2ImagePath !== null) {
+            suscriber.referee2ImagePath = _suscriber.referee2ImagePath
+        }
+
+        if (_suscriber.referee1ImagePath !== "" || _suscriber.referee1ImagePath !== null) {
+            suscriber.referee1ImagePath = _suscriber.referee1ImagePath
+        }
+
+
+        const editedSuscriber = await suscriber.save();
+        // console.log(editedSuscriber)
+        return res.status(200).json({});
+
     }
+
     catch (err) {
         res.status(500).json({
             errorMessage: " error getting suscriber from database...."
@@ -27,15 +140,16 @@ const editMonthlySuscriber = async (req, res) => {
 
 }
 
+
+
 const editCustomerImage = async (req, res) => {
     try {
-        // const suscriber = await Suscriber.findById(req.params.id);
-        // if (!suscriber)
-        //     return res.status(500).json({
-        //         errorMessage: "can not find users"
-        //     })
-        console.log("edit customer images"+req.params.id)
-        return res.status(200).json();
+        if(req.file !==null || req.file !==undefined){
+            let customerImagePath = req.file.path;
+            _suscriber.customerImagePath = customerImagePath;
+            return res.status(200).json();
+        }
+      
     }
     catch (err) {
         res.status(500).json({
@@ -50,15 +164,15 @@ const editCustomerImage = async (req, res) => {
 
 const editRef2Image = async (req, res) => {
     try {
-        // const suscriber = await Suscriber.findById(req.params.id);
-        // if (!suscriber)
-        //     return res.status(500).json({
-        //         errorMessage: "can not find users"
-        //     })
-        // return res.status(200).json(suscriber);
 
-        console.log("EDIT REF2"+req.params.id)
-        return res.status(200).json();
+        if(req.file !==null || req.file !==undefined){
+            let referee2ImagePath = req.file.path;
+            _suscriber.referee2ImagePath = referee2ImagePath;
+            console.log("edit 2 is fired")
+            return res.status(200).json();
+        }
+
+        return null
     }
     catch (err) {
         res.status(500).json({
@@ -73,16 +187,15 @@ const editRef2Image = async (req, res) => {
 
 const editRef1Image = async (req, res) => {
     try {
-        // const suscriber = await Suscriber.findById(req.params.id);
-        // if (!suscriber)
-        //     return res.status(500).json({
-        //         errorMessage: "can not find users"
-        //     })
-        // return res.status(200).json(suscriber);
 
+        if(req.file !==null || req.file !==undefined){
+            let referee1ImagePath = req.file.path;
+            _suscriber.referee1ImagePath = referee1ImagePath;
+            console.log("edit 1 is fired")
 
-        console.log("editREEF1"+req.params.id)
-        return res.status(200).json();
+            return res.status(200).json();
+        }
+        return null
     }
     catch (err) {
         res.status(500).json({
@@ -93,6 +206,11 @@ const editRef1Image = async (req, res) => {
     }
 
 }
+
+
+
+
+
 
 module.exports = {
     editRef1Image,
